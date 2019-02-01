ea6c0c2b603e12fde9c1c2cec59024af915364e2
Ethan Brimhall <ethannbrimhall@gmail.com>
2019-02-01 17:42:49 -0500

    Added ability to create a 'Shorty' URL

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+mongoose.connect(config.database);
+let db = mongoose.connection;
+
+let Url = require('./models/url');
+
+// Check DB connection
+db.once('open', () =>{
+  console.log('connected to mongoDB');
+});
+
+// Check for DB errors
+db.on('error', () =>{
+  console.log(err);
+});
 
 const app = express();
 
@@ -21,8 +37,40 @@ app.get('/', (req, res) =>{
 });
 
 app.post('/', (req, res) =>{
-  console.log(req.body.url);
-  res.render('index');
+
+  var url = req.body.url;
+
+  Url.findOne({urlAddress:url}, (err, theUrl) => {
+    if(err) throw err;
+
+    if(theUrl){
+      return res.render('index', {theUrl:theUrl.shortyCode});
+    }else{
+      var text = "";
+      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      for (var i = 0; i < 5; i++){
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+      // Check if text already exists
+      let newUrl = new Url({
+        urlAddress: url,
+        shortyCode: text
+      });
+
+      newUrl.save((err) => {
+        if(err){
+          console.log(err);
+          return;
+        }else{
+          return res.render('index', {theUrl:text});
+        }
+
+      });
+    }
+
+  });
+
+
 });
 
 app.listen(3000, () => {
